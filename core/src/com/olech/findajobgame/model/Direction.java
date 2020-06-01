d9601640c8970fa29c466fad328c6cd85498f528
marcinolech <mrc.olech@gmail.com>
2020-06-01 12:05:42 +0200

    added oppositeDirection() method

--- core/src/com/olech/findajobgame/model/Direction.java
+++ core/src/com/olech/findajobgame/model/Direction.java
@@ -4,5 +4,19 @@ public enum Direction {
     UP,
     DOWN,
     LEFT,
-    RIGHT
+    RIGHT;
+
+    public Direction getOpposite() {
+        switch (this){
+            case DOWN:
+                return UP;
+            case UP:
+                return DOWN;
+            case LEFT:
+                return RIGHT;
+            case RIGHT:
+                return LEFT;
+        }
+        return this;
+    }
 }
